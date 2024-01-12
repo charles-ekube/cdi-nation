d4f823913093a1786b7a189bdeb24196beb3a1f7
charles-ekube <charleswakili797@gmail.com>
2024-01-12 12:11:10 +0100

    fix:working on hero section

--- src/components/home/HeroSection.js
+++ src/components/home/HeroSection.js
@@ -1,44 +1,50 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import HeroCarousel from './HeroCarousel';
+import CustomInput from '../../utils/CustomInput';
+import Button from '../../utils/CustomButton';
+
 
 const HeroSection = () => {
+    // const isTabletOrMobile = useMediaQuery({ query: '(min-width: 1224px)' })
 
+    // const text = 'Welcome to The CDI Church';
+    // const delay = 200;
+    // const infinite = true;
 
-    const text = 'Welcome to The CDI Church';
-    const delay = 200;
-    const infinite = true;
+    // const [currentText, setCurrentText] = useState('');
+    // const [currentIndex, setCurrentIndex] = useState(0);
 
-    const [currentText, setCurrentText] = useState('');
-    const [currentIndex, setCurrentIndex] = useState(0);
+    // useEffect(() => {
+    //     let timeout;
 
-    useEffect(() => {
-        let timeout;
+    //     if (currentIndex <= text.length) {
+    //         timeout = setTimeout(() => {
+    //             setCurrentText(prevText => prevText + text[currentIndex]);
+    //             setCurrentIndex(prevIndex => prevIndex + 1);
+    //         }, delay);
 
-        if (currentIndex <= text.length) {
-            timeout = setTimeout(() => {
-                setCurrentText(prevText => prevText + text[currentIndex]);
-                setCurrentIndex(prevIndex => prevIndex + 1);
-            }, delay);
+    //     } else if (infinite) { // ADD THIS CHECK
+    //         setCurrentIndex(0);
+    //         setCurrentText('');
+    //     }
 
-        } else if (infinite) { // ADD THIS CHECK
-            setCurrentIndex(0);
-            setCurrentText('');
-        }
+    //     return () => clearTimeout(timeout);
+    // }, [currentIndex, delay, infinite, text]);
 
-        return () => clearTimeout(timeout);
-    }, [currentIndex, delay, infinite, text]);
 
 
     return (
         <section className={'heroSection'}>
             <header>
                 <div>
-                    <h2 className={'boldText'}>Welcome to the Cdi Church</h2>
-                    <p className={'semiBoldText'}>We are the inspiration people who are been raised by God to take over nations, cities, territories etc.</p>
+                    <h2 className={`boldText`}>Welcome to  <span>the Cdi Church</span></h2>
+                    <p className={'semiBoldText'}>
+                        We are the inspiration people who are been raised by God to take over nations, cities, territories etc.
+                    </p>
                 </div>
                 <div className={'heroInputContainer'}>
-                    <input placeholder='Enter your email address' />
-                    <button>Get notified</button>
+                    <CustomInput />
+                    <Button text={'Get notified'} className={'heroInputButton'} />
                 </div>
             </header>
             <HeroCarousel />
--- src/utils/CustomButton.js
+++ src/utils/CustomButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Text from './CustomText';
 
 const Button = ({ text, onClick, className, loading, ...otherProps }) => {
-    const combinedClassName = `customButton text semiBoldText f16 ${className}`.trim(); // Combine the general className and the passed className
+    const combinedClassName = `semiBoldText f16 ${className}`.trim(); // Combine the general className and the passed className
 
     return (
         <button className={combinedClassName} onClick={onClick} {...otherProps}>
